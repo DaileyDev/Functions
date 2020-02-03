54650ef1bf5cf03f6b309ace497b23db43d6a2fe
Kayla Dailey <kayladailey000@gmail.com>
2020-02-02 22:34:21 -0600

    Initial Commit and file layout

---
+++ tutorial/src/fundamentals/Syntax.js
@@ -0,0 +1,12 @@
+//Parameters
+
+
+
+
+//Arguments
+
+
+
+// The return Statement
+// When you see a return in a block of code within a function, keep in mind that anything written after a return statement will not execute. This is because the return statment will terminate
+//  the function after running the line of statements within it.
